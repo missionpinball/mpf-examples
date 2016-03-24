42dc9534b81ddb99e7863af7e66c03f8697f742c
Jan Kantert <kantert@sra.uni-hannover.de>
2016-03-24 01:22:18 +0100

    very basic machine test

---
+++ demo_man/tests/test_simple_game.py
@@ -0,0 +1,10 @@
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+
+class TestSimpleGame(MpfMachineTestCase):
+
+    def get_platform(self):
+        return 'smart_virtual'
+
+    def testLoadConfig(self):
+        pass
