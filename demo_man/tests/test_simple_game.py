96c18be431c237b526a87fb2175688b2476d081d
Jan Kantert <jan-mpf@kantert.net>
2016-05-11 01:29:41 +0200

    extend test to start a real game

--- demo_man/tests/test_simple_game.py
+++ demo_man/tests/test_simple_game.py
@@ -6,5 +6,26 @@ class TestSimpleGame(MpfMachineTestCase):
     def get_platform(self):
         return 'smart_virtual'
 
-    def testLoadConfig(self):
-        pass
+    def testSimpleGame(self):
+        self.hit_and_release_switch("s_start")
+        self.advance_time_and_run(20)
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(1, self.machine.game.num_players)
+        self.assertEqual(1, self.machine.game.player.ball)
+
+        self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
+        self.advance_time_and_run(20)
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(1, self.machine.game.num_players)
+        self.assertEqual(2, self.machine.game.player.ball)
+
+        self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
+        self.advance_time_and_run(20)
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(1, self.machine.game.num_players)
+        self.assertEqual(3, self.machine.game.player.ball)
+
+        self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
+        self.advance_time_and_run(20)
+        self.assertIsNone(self.machine.game)
+        self.assertEqual(0, self.machine.playfield.balls)
