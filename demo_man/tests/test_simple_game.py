5e47c2c51380787c19a4a5be308aa072c5b341e7
Jan Kantert <jan-mpf@kantert.net>
2016-09-20 00:04:51 +0200

    extend tests

--- demo_man/tests/test_simple_game.py
+++ demo_man/tests/test_simple_game.py
@@ -1,7 +1,7 @@
-from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+from mpf.tests.MpfMachineTestCase import FullMachineTestCase
 
 
-class TestSimpleGame(MpfMachineTestCase):
+class TestSimpleGame(FullMachineTestCase):
 
     def test_single_player_game(self):
         self.hit_and_release_switch("s_start")
@@ -16,6 +16,17 @@ class TestSimpleGame(MpfMachineTestCase):
         # after 20 its there
         self.advance_time_and_run(20)
         self.assertEqual(1, self.machine.playfield.balls)
+
+        self.assertTextOnTopSlide("BALL 1    FREE PLAY")
+
+        # make some points
+        self.hit_and_release_switch("s_left_jet")
+        self.hit_and_release_switch("s_left_jet")
+        self.hit_and_release_switch("s_left_jet")
+        self.hit_and_release_switch("s_left_jet")
+        self.advance_time_and_run()
+        self.assertEqual(4 * 75020, self.machine.game.player.score)
+        self.assertTextOnTopSlide("300,080")
 
         # ball drains
         self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
@@ -41,5 +52,19 @@ class TestSimpleGame(MpfMachineTestCase):
 
         # ball drains again. game should end
         self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
+        self.advance_time_and_run(10)
+
+        # enter high score
+        self.assertSlideOnTop("high_score_enter_initials")
+        self.hit_and_release_switch("s_flipper_lower_right")
+        self.hit_and_release_switch("s_flipper_lower_right")
+        self.hit_and_release_switch("s_start")  # C
+        self.advance_time_and_run()
+        self.hit_and_release_switch("s_flipper_lower_right")
+        self.hit_and_release_switch("s_start")  # CD
+        self.advance_time_and_run()
+        self.hit_and_release_switch("s_flipper_lower_right")
+        self.hit_and_release_switch("s_start")  # CDE
+
         self.advance_time_and_run(10)
         self.assertIsNone(self.machine.game)
