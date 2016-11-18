3a6797ad8ee26a10049b2b50e895de033c823b89
Brian Madden <brian@missionpinball.com>
2016-11-18 12:32:18 -0800

    preparing games for unit tests

---
+++ jd/tests/test_simple_game.py
@@ -0,0 +1,10 @@
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+
+class TestSimpleGame(MpfMachineTestCase):
+
+    def getMachinePath(self):
+        return "jd"
+
+    def testLoadConfig(self):
+        pass
---
+++ starter_game/tests/test_simple_game.py
@@ -0,0 +1,10 @@
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+
+class TestSimpleGame(MpfMachineTestCase):
+
+    def getMachinePath(self):
+        return "start_game"
+
+    def testLoadConfig(self):
+        pass
