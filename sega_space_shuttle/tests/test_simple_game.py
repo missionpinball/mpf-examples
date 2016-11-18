8576555b2ca9320a662c9a74fe976920610afd3c
Brian Madden <brian@missionpinball.com>
2016-11-18 12:32:05 -0800

    added sega_space_shuttle

---
+++ sega_space_shuttle/tests/test_simple_game.py
@@ -0,0 +1,10 @@
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+
+class TestSimpleGame(MpfMachineTestCase):
+
+    def getMachinePath(self):
+        return "sega_space_shuttle"
+
+    def testLoadConfig(self):
+        pass
