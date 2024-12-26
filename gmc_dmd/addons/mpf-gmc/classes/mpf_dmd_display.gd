5fe925e18eb9484b89cff6aae1542647fd69fa95
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-12-26 15:45:49 -0800

    Update DMD implementation

--- gmc_dmd/addons/mpf-gmc/classes/mpf_dmd_display.gd
+++ gmc_dmd/addons/mpf-gmc/classes/mpf_dmd_display.gd
@@ -1,7 +1,23 @@
 class_name MPFDMDDisplay
 extends MPFDisplay
 
+enum PixelOrder {
+	RGB,
+	RBG,
+	GRB
+}
+
+enum GPUStrategy {
+	PRODUCTION,
+	ALWAYS,
+	NEVER
+}
+
 @export var resolution: Vector2i
+@export var pixel_order: PixelOrder = PixelOrder.RGB
+@export var use_gpu: GPUStrategy = GPUStrategy.NEVER
+
+var _rgb_shift_fn: Callable
 
 func _ready() -> void:
 	super()
@@ -13,26 +29,48 @@ func _ready() -> void:
 	# the window is not visible. Requires Godot 4.3
 	DisplayServer.register_additional_output(self)
 
+	if use_gpu == GPUStrategy.ALWAYS or (use_gpu == GPUStrategy.PRODUCTION and OS.has_feature("template")):
+		self.log.warning("GPU-based DMD shading is in progress, please inquire!")
+	# Until GPU is automated, always use software
+	if pixel_order == PixelOrder.RBG:
+		_rgb_shift_fn = self._shift_rbg
+	elif pixel_order == PixelOrder.GRB:
+		_rgb_shift_fn = self._shift_grb
+
 func _exit_tree() -> void:
 	DisplayServer.unregister_additional_output(self)
 
 func _pre_draw():
+	# If the drawing changes, wait for the new draw and capture
 	if RenderingServer.has_changed():
-		print(" - draw is changed?")
 		await RenderingServer.frame_post_draw
 		self._capture()
-		print(" - draw is done?")
-
 
 func _capture() -> void:
-	var tex := get_viewport().get_texture().get_image()
-	tex.crop(resolution.x, resolution.y)
-	var color = tex.get_pixelv(Vector2i(0, 0))
-	var data = tex.get_data()
+	# Ignore draw updates if no BCP is connected
 	if not MPF.server._client:
-		print("no client")
 		return
-	# MPF.server.send_event_with_args("rgb_dmd_frame", {"rawbytes": data, "name": self.name})
+	var tex := get_viewport().get_texture().get_image()
+	# Downsize the image to the size of the DMD, no interpolation
+	tex.resize(resolution.x, resolution.y, 0)
+	# If RGB shifting is needed, do it now
+	self._rgb_shift_fn and self._rgb_shift_fn.call(tex)
+	var data = tex.get_data()
+	# !!! Due to MPF BCP parsing limitations 'bytes' MUST be the last arg
 	MPF.server._send("rgb_dmd_frame?name=%s&bytes=%d" % [name, data.size()])
 	MPF.server._client.put_data(data)
-	# MPF.server._client.put_data("\n".to_ascii_buffer())
+
+func _apply_shader() -> void:
+	pass
+
+func _shift_grb(tex: Image) -> void:
+	for y in range(resolution.y):
+		for x in range(resolution.x):
+			var color = tex.get_pixel(x, y)
+			tex.set_pixel(x, y, Color(color.g, color.r, color.b, color.a))
+
+func _shift_rbg(tex: Image) -> void:
+	for y in range(resolution.y):
+		for x in range(resolution.x):
+			var color = tex.get_pixel(x, y)
+			tex.set_pixel(x, y, Color(color.r, color.b, color.g, color.a))
